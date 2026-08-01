3fc42d1e7297df0cf324ee016f93a525be928616
Nitin Dhyani <nitindhyani1234@gmail.com>
2026-08-01 22:03:42 +0530

    Basic String

---
+++ Str.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+class Str{
+    public static void main(String[] args) {
+    //     String name= "Nitin";
+    //     String Fullname = name.concat(" Dhyani");
+    //     System.out.println(Fullname);
+    //     int strlen=Fullname.length();
+    //     System.out.println("String Length is "+strlen);
+    // }
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter String :");
+    String str= sc.nextLine();
+    System.out.println("String is: "+str);
+}
+}
+
