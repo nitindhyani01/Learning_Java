9da4a0c8ca0b4079c42d22259acb137e132ba14c
Nitin Dhyani <nitindhyani1234@gmail.com>
2026-09-10 20:11:37 +0530

    MergeSort

--- CyclicSort.java
+++ CyclicSort.java
@@ -1,14 +1,10 @@
-
 import java.util.Arrays;
-
 class CyclicSort {
-
     public static void main(String[] args) {
         int[] arr = {5, 2, 3, 1, 4};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
-
     public static void sort(int[] arr) {
         int i = 0;
         while (i < arr.length) {
@@ -20,7 +16,6 @@ class CyclicSort {
             }
         }
     }
-
     public static void swap(int[] arr, int i, int correct) {
         int temp;
         temp = arr[i];
