7b73a04ef4d6600e03ba22f95d653e2f2ee59368
Nitin Dhyani <nitindhyani1234@gmail.com>
2026-09-11 22:43:02 +0530

    UniqueThreeDigitEven program

---
+++ UniqueThreeDigitEven.java
@@ -0,0 +1,34 @@
+class UniqueThreeDigitEven {
+    public static void main(String[] args) {
+        int[] digits = {6, 6, 6};
+        System.out.print(totalNumbers(digits));
+    }
+
+    public static int totalNumbers(int[] digits) {
+        int count = 0;
+        int[] freq = new int[10];
+        for (int d : digits) {
+            freq[d]++;
+        }
+        for (int i = 1; i <= 9; i++) {
+            if (freq[i] == 0) {
+                continue;
+            }
+            freq[i]--;
+            for (int j = 0; j <= 9; j++) {
+                if (freq[j] == 0) {
+                    continue;
+                }
+                freq[j]--;
+                for (int k = 0; k <= 8; k += 2) {
+                    if (freq[k] > 0) {
+                        count++;
+                    }
+                }
+                freq[j]++;
+            }
+            freq[i]++;
+        }
+        return count;
+    }
+}
